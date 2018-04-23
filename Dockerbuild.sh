3c18ad156885d49b0999d455be33c24ff0d45b0f
Ai Nguyen <ainguyenkaka@gmail.com>
2018-04-23 16:55:31 +0800

    update deps

--- Dockerbuild.sh
+++ Dockerbuild.sh
@@ -1,2 +1,3 @@
+CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o main.out .
 docker build -t gonitor/gonitor-websocket .
 docker tag  gonitor/gonitor-websocket gonitor/gonitor-websocket:0.0.0
